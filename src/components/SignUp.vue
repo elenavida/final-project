fb356b8e468364411ef6dd3600d9b759b2a2dae2
Elena Vida <elenavidaluc3a@gmail.com>
2022-08-20 13:32:10 +0200

    add missing features sign up

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -20,10 +20,12 @@
     />
     <button type="submit">Sign Up</button>
   </form>
+  <p v-if="errorMessage">{{ errorMessage }}</p>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
 import { useUserStore } from "../stores/user";
 import PersonalRouter from "./PersonalRouter.vue";
 
@@ -38,21 +40,36 @@ const email = ref();
 const password = ref();
 
 // Error Message
-
+const errorMessage = ref("");
 // Show hide password variable
-
+const passwordFieldType = computed(() =>
+  hidePassword.value ? "password" : "text"
+);
+const hidePassword = ref(true);
 // Show hide confrimPassword variable
 
 // Router to push user once SignedUp to Log In
-
+const redirect = useRouter();
 // Arrow function to SignUp user to supaBase with a timeOut() method for showing the error
 async function signUp() {
-  const response = await userStore
-    .signUp(email.value, password.value)
-    .catch((error) => {
-      alert(error.message);
-    });
-  alert(`success! ${response.email}`);
+  //   const response = await userStore
+  //     .signUp(email.value, password.value)
+  //     .catch((error) => {
+  //       alert(error.message);
+  //     });
+  try {
+    // calls the user store and send the users info to backend to logIn
+    await userStore.signUp(email.value, password.value);
+    // redirects user to the homeView
+    redirect.push({ path: "/" });
+  } catch (error) {
+    // displays error message
+    errorMessage.value = `Error: ${error.message}`;
+    // hides error message
+    setTimeout(() => {
+      errorMessage.value = null;
+    }, 5000);
+  }
 }
 </script>
 
